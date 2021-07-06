d688882b3d7cf0a5321b37413cfac09f0ae35006
SujitAdhikari <sujit.cse.bd@gmail.com>
2021-07-07 01:45:01 +0600

    default

---
+++ Todolist.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+
+exports default class TodoList extends React.Component{
+    state = {
+        todos: []
+    }
+    componentDidMount(){
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const todos = res.data;
+        this.setState({ todos });
+      })
+    }
+    render() {
+        return (
+          <ul>
+            { this.state.todos.map(person => <li>{todos.name}</li>)}
+          </ul>
+        )
+      }
+}
