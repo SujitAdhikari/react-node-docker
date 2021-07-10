7295907c2bc4286a3309bc8e5100baf90b073837
Sujit Kumar Adhikari <82542326+SujitAdhikari@users.noreply.github.com>
2021-07-10 17:07:06 +0600

    Update Todolist.js

--- frontend/src/components/Todolist.js
+++ frontend/src/components/Todolist.js
@@ -6,8 +6,8 @@ export default class TodoList extends React.Component{
         todos: []
     }
     componentDidMount(){
-      //  axios.get(`https://jsonplaceholder.typicode.com/users`)
-      axios.get(`http://localhost:3030/api/v1/healthz`)
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+      //axios.get(`http://localhost:3030/api/v1/healthz`)
       .then(res => {
         const todos = res.data;
         this.setState({ todos });
